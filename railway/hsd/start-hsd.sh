c4492d1f12f4048b0fda5458c4a46d842941c024
shadstoneofficial <contact@shadstone.com>
2026-05-09 14:43:03 +0700

    Improve Railway HSD startup diagnostics

--- railway/hsd/start-hsd.sh
+++ railway/hsd/start-hsd.sh
@@ -8,16 +8,19 @@ fi
 
 mkdir -p "${HSD_PREFIX}"
 
+echo "Starting HSD ${HSD_NETWORK} on ${HSD_HTTP_HOST}:${HSD_HTTP_PORT} with prefix ${HSD_PREFIX}"
+
 exec env -i \
   HOME=/root \
   NODE_ENV=production \
   PATH="/opt/hsd/bin:/opt/hsd/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
-  node /opt/hsd/bin/hsd \
+  node /opt/hsd/bin/node \
   --network="${HSD_NETWORK}" \
   --prefix="${HSD_PREFIX}" \
   --http-host="${HSD_HTTP_HOST}" \
   --http-port="${HSD_HTTP_PORT}" \
   --api-key="${HSD_API_KEY}" \
   --no-wallet \
+  --log-console=true \
   --log-level="${HSD_LOG_LEVEL}" \
   ${HSD_EXTRA_ARGS:-}
